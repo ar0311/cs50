2469b5bfad4ba2ebd12938d7b2d48ddb2e136176
John Harvard <arogers0311@gmail.com>
2014-07-19 19:41:00 -0400

    formatting

--- ps1/mario.c
+++ ps1/mario.c
@@ -4,36 +4,29 @@
 int main(void)
 { 
     int height;
+
     while (height>23 || height<=0) 
     {
         printf("Height: ");
         height = GetInt();
     }
-    // below is leftovers from trying to use masks instead of loops to print the pyramid
-    //
-    //while (height > 0)
-    //{
-    //        printf("%*s""%*s\n", (height - 1), "", blocks, "#");
-    //        height--;
-            //blocks++;
 
-            //printf("%*s\n", blocks, "#");
-    //}
-    
     for(int i = 0; i < height; i++) 
 	{
-		// print the spaces
+	
         for(int j = 0; j < height-i-1; j++)
         {
             printf(" ");
         }
-        // print the hashes
+        
 		for(int k = 0; k < i+2; k++)
 		{
 			printf("#");
 		}
+		
 		printf("\n");
 	}
+	
 	return 0;    
 }
 
