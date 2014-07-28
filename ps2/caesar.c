686989875b7a0cefe50be7143c8340eebf6bd449
John Harvard <arogers0311@gmail.com>
2014-07-28 04:25:21 -0400

    started argument parsing and validation

--- ps2/caesar.c
+++ ps2/caesar.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 #include <cs50.h>
 
-
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Invalid number of command line arguments entered.\n\n");
+        printf("Usage: caesar <key>\n");
+        printf("where <key> is a numeric key between 1 and 26.\n\n"); 
+    }
+}
