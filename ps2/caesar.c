11d9c162ea59ce21d71d851dea98ca368d7f3e84
John Harvard <arogers0311@gmail.com>
2014-07-28 04:50:29 -0400

    more work on validation, removed 26 condition

--- ps2/caesar.c
+++ ps2/caesar.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Invalid number of command line arguments entered.\n\n");
-        printf("Usage: caesar <key>\n");
-        printf("where <key> is a numeric key between 1 and 26.\n\n"); 
+        printf("\nInvalid command line arguments entered.\n\n");
+        printf("Usage: ./caesar <key>\n");
+        printf("where <key> is a positive numeric value.\n\n");
+        return 1; 
     }
+    
+    int key = atoi(argv[1]);
+    
+    if (key <= 0)
+    {
+        printf("\nInvalid key entered.\n\n");
+        printf("Usage: ./caesar <key>\n");
+        printf("where <key> is a positive numeric value.\n\n");
+        return 1;
+    }
+    
+    printf("%d\n",key);
 }
