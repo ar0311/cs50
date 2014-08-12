ed04890b93ae88b20a16352ff21567b440827762
John Harvard <arogers0311@gmail.com>
2014-08-12 06:34:16 -0400

    implement linear search in helpers.c

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -16,7 +16,17 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+    if (n <= 0)
+    {
+        return false;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (values[i] == value)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
