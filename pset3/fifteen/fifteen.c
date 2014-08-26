f7b35c11701f7856ea985b85d8f040f1d519f65b
John Harvard <arogers0311@gmail.com>
2014-08-26 07:11:24 -0400

    fix draw compilation errors

--- pset3/fifteen/fifteen.c
+++ pset3/fifteen/fifteen.c
@@ -146,7 +146,7 @@ void draw(void)
     for (int i = 0; i < d; i++)
     {
         for (int j = 0; j < d; j++)
-            printf("|%d|",i,j)
+            printf("|%d|",board[i][j]);
         printf("\n");
     }
 }
